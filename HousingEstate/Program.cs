9d0eaf86b650029b4657a6b49992b7414c05fad4
maki <jantulamarko@outlook.com>
2022-12-10 14:31:48 +0100

    idk what i created.

--- HousingEstate/Program.cs
+++ HousingEstate/Program.cs
@@ -6,7 +6,13 @@ namespace HousingEstate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person Peter = new Person("Peter", "Sovík", 50);
+            Person Oliver = new Person("Oliver", "Krška", 17);
+            Person Lucian = new Person("Lucián", "Švancár", 17);
+
+            Console.WriteLine(Peter.ToString());
+            Console.WriteLine(Oliver.ToString());
+            Console.WriteLine(Lucian.ToString());
         }
     }
 }
