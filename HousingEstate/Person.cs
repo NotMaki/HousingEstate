da8f6da748fa23d442fb2992a9aceb80d2861503
maki <jantulamarko@outlook.com>
2022-12-10 14:22:29 +0100

    Add project files.

---
+++ HousingEstate/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingEstate
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+        private string surname;
+
+        public Person(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"Name: {name}\nSurname: {surname}\nAge: {age}\n");
+        }
+    }
+}
